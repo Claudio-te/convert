83ed08644b3bee4fb176197532c69db3843c839d
claudio.te <claudio.souza.te@gmail.com>
2025-04-08 20:20:17 -0300

    formatando o total

--- scripts.js
+++ scripts.js
@@ -7,6 +7,7 @@ const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
 const description = document.getElementById("description")
+const result = document.getElementById("result")
 
 amount.addEventListener("input",  () => {
   
@@ -34,11 +35,19 @@ form.onsubmit = (event) => {
 
 function convertCurrency(amount, price, symbol) {
  try {
-
    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`
 
-   footer.classList.add("show-result")
+  let total = amount * price 
 
+  if (isNaN(total)) {
+    return alert ("Por favor, digite o valor corretamente para converter.")
+  }
+
+  total = formatCurrencyBRL(total).replace("R$", "")
+
+  result.textContent = `${total} Reais`
+
+   footer.classList.add("show-result")
  } catch (error) {
   
   footer.classList.remove("show-result")
